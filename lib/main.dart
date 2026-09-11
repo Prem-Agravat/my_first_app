66d4c15b4903a7e4924ca126096e64a63532c2b3
Prem Agravat <agravatprem00@gmail.com>
2026-09-11 08:33:26 +0530

    Add SliderExample widget for slider functionality and update main app

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:my_first_app/inputcontrol/chkbox.dart';
 import 'package:my_first_app/inputcontrol/inputtextcontrol.dart';
 import 'package:my_first_app/inputcontrol/radiobutton.dart';
+import 'package:my_first_app/inputcontrol/sliderexample.dart';
 
 void main() {
   runApp(const StopWatchRun());
@@ -13,6 +14,9 @@ class StopWatchRun extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: ChkBoxExam());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SliderExample(),
+    );
   }
 }
